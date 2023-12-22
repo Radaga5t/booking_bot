42643695659bcd05f9337ff7577278b28cb03dc0
shamil9321 <ilya252752@gmail.com>
2023-12-23 01:45:04 +0700

    Сделано,подключено,user создался

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
 from os import getenv
 from telegram import Bot
 from flask_sqlalchemy import SQLAlchemy
-from telegram.ext import Updater, CommandHandler
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask
 
-app = Flask(__name__)
+
 
 load_dotenv()
-TELEGRAM_BOT_TOKEN = getenv('bot_token')
-bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://booking_bot:252752@localhost/postgres'
+bot = Bot(token=getenv('TOKEN'))
+app = Flask(__name__)
+db = SQLAlchemy()
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
 
 # Ваша модель User
 class User(db.Model):
@@ -22,13 +23,15 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(50))
 
+db.init_app(app)
+
 # Создание всех таблиц
 with app.app_context():
     db.create_all()
 
 @app.route('/')
 def index():
-    return "pisa_popa_chelen"
+    return 'pisa_popa_chelen'
 
 if __name__ == '__main__':
     app.run()
