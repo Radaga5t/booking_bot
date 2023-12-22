99d937ef0371d27fd2353d9c80158c9d384f50d6
noticeqqw <127593031+noticeqqw@users.noreply.github.com>
2023-12-23 01:46:27 +0700

    Добавление обработчика ошибок

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from os import getenv
 from telegram import Bot
 from flask_sqlalchemy import SQLAlchemy 
@@ -24,6 +24,27 @@ class User(db.Model):
 db.init_app(app)
 with app.app_context():
     db.create_all()
+
+#----------------------------------------------------------------
+    
+# Обработчик ошибки 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify(error=str(e)), 404
+
+
+# Обработчик ошибки 500
+@app.errorhandler(500)
+def server_error(e):
+    return jsonify(error="Внутренняя ошибка сервера"), 500
+
+
+# Обработчик для неопределенных ошибок
+@app.errorhandler(Exception)
+def handle_exception(e):
+    return jsonify(error="Непредвиденная ошибка"), 500
+
+
 
 @app.route('/')
 def index():
