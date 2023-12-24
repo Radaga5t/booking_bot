dfb928bf3c1c1e06c0bb9bcb0076688e637bb088
noticeqqw <127593031+noticeqqw@users.noreply.github.com>
2023-12-24 22:36:03 +0700

    Добавление миграций

--- app.py
+++ app.py
@@ -19,17 +19,11 @@ app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 #----------------------------------------------------------------Models
 
-attendees = db.Table('attendees', db.Model.metadata,
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True)
-)
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
-    events_attending = db.relationship('Event',secondary=attendees, back_populates="attendees")
-
+    
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
@@ -37,12 +31,18 @@ class Event(db.Model):
     start_time = db.Column(db.DateTime)
     end_time = db.Column(db.DateTime)
     chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'), nullable=False)
-    attendees = db.relationship('User', secondary=attendees, back_populates='events_attending')
 
 class Chat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     chat_identifier = db.Column(db.String(120), unique=True, nullable=False)
     events = db.relationship('Event', backref='chat', lazy=True)
+
+class Attende(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    user = db.relationship('User', back_populates='events')
+    event = db.relationship('Event', back_populates='attendees')
 
 #----------------------------------------------------------------Table
     
